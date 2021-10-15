ac9ec0f61600770520aae7b07b1d9552485771c5
FSIce1 <luisfelipesiesquen22@gmail.com>
2021-10-15 14:07:03 -0500

    Subir plantilla

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      colors: {
+        'primary': '#f3c614',
+        'secondary': '#353535'
+      },
+      height: {
+        '80': '20rem',
+        '100': '25rem'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
